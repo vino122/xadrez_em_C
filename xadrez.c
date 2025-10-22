e0b189b761a3dc5be450f896bd3ab15d88032e1e
Vinicius Felipe de Araujo <viniciusfelipe.felip@gmail.com>
2025-10-22 13:54:37 -0300

    Refactor chess piece movement functions

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,71 @@
 #include <stdio.h>
-int main() {
 
+void mover_bispo(int movimentos)
+{
+    for (int i = 0; i < movimentos; i++)
+    {
+        printf("diagonal\n");
+    }
+}
+
+void mover_torre(int movimentos)
+{
+    int i = 0;
+    do
+    {
+        printf("Frente\n");
+        i++;
+    } while (i < movimentos);
+}
+
+void mover_rainha(int movimentos)
+{
+    int rainha = 0;
+    while (movimentos > 0)
+    {
+        printf("Frente\n");
+        movimentos--;
+    }
     
+}
+
+int main()
+{
+    // Movimento do bispo
+    int movimentos;
+
+    printf("Digite o número de movimentos do bispo: (1-5)\n");
+    scanf("%d", &movimentos);
+    printf("BISPO\n");
+    mover_bispo(movimentos);
+
+    printf("\n");
+
+    // Movimento da torre
+    printf("TORRE\n");
+    mover_torre(5);
+
+    printf("\n");
+
+    // Movimento da rainha
+    printf("RAINHA\n");
+    mover_rainha(8);
+
+    printf("\n");
+
+    // Movimento do cavalo
     int cavalo = 1;
 
     printf("CAVALO\n");
-    while (cavalo--)
+    while (cavalo > 0)
     {
-        for ( int i = 0; i < 2; i++) {
-            printf("Baixo\n"); //imprime Baixo 1 vez
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Cima\n"); // imprime Cima 1 vez
         }
-        printf("Esquerda\n");
+        printf("Direita\n");
+        cavalo--;
     }
-    
-  
-    printf("\n");
-
-    printf("BISPO\n");
-    int bispo = 0;
-    while (bispo < 5) {
-        printf("Diagonal\n"); //imprime Diagonal 5 vezes
-        bispo++;
-    }
-
-    printf("\n");
-
-    int torre = 0;
-    printf("TORRE\n");
-    do {
-        printf("Frente\n"); //imprime Frente 5 vezes
-        torre++;
-    } while (torre < 5);
-
-
-
-
-
-
-
-
 
     return 0;
 }
-
-
